8fceec10cf865101cc7c5f4a4a17c9ca3b388aa8
anil.ozler <anil.ozler@KZL-YZLMORT-B11>
2022-03-26 19:02:08 +0300

    Add project files.

---
+++ o1_ekranKoruyucu/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace o1_ekranKoruyucu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //rastgele olarak ekranda dolaşmaslı gerekli
+        Random rnd = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // rnd.Next(0, 100);
+          pictureBox1.Left = rnd.Next(0, this.Width - pictureBox1.Width);
+            pictureBox1.Top = rnd.Next(0, this.Height - pictureBox1.Height);
+
+        }
+    }
+}
